812271b8bccc0050e25cdf6af5b896f0cb41e6f5
unknown <you@example.com>
2021-02-18 20:13:47 +0700

    pratikum 02 membuat objek dan class

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include('Mahasiswa.php');
+
+$siauling = new Mahasiswa();
+$lita = new Mahasiswa();
+$harry = new Mahasiswa();
+
+$siauling->nim ='H1101191009';
+$siauling->nama ='Siauling';
+$siauling->tanggal_lahir ='02 Januari 2001';
+$siauling->jenis_kelamin ='Perempuan';
+
+$lita->nim ='H1101191058';
+$lita->nama ='Novitasari';
+$lita->tanggal_lahir ='9 November 2001';
+$lita->jenis_kelamin ='Perempuan';
+
+$harry->nim ='H1101191040';
+$harry->nama ='Harry Anshary';
+$harry->tanggal_lahir ='31 Maret 2001';
+$harry->jenis_kelamin ='Laki-laki';
+
+
+
