0ee6044bc80e8d083cd22b1d96b5a13a28a32ca5
unknown <you@example.com>
2021-04-24 13:45:39 +0700

    Pratikum Polimorfisme

---
+++ learn_overriding.php
@@ -0,0 +1,33 @@
+<?php
+
+class Mahasiswa
+{
+    public $nama;
+
+    public function sapa()
+    {
+        echo "Selamat kepada ".$this->nama. " atas terpilihya sebagai ketua himpunan SISFO\n";
+    }
+
+    public function masuk ()
+    {
+        echo "Masa periode anda terhitung sejak " . date('Y-m-d') . "\n";
+    }
+}
+
+class KetuaJurusan extends Mahasiswa
+{
+    public function sapa()
+    {
+        echo"Selamat " . $this->nama ." semoga dapat menjalani tanggung jawab dengan baik\n";
+    }
+    public function parentSapa()
+    {
+        return parent::sapa();
+    }
+}
+
+$okis = new KetuaJurusan ();
+$okis-> nama = 'Kristian Hermawan';
+$okis->parentSapa();
+$okis->masuk();
